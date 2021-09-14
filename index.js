9782b1ecf02890e314a7c300deaecc4f89efb9b4
Riccardo Di Benedetto <dibe.ricky@gmail.com>
2021-09-14 20:43:25 +0200

    feat: upsert on mongo when a msg is received

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 'use strict'
 
+require('dotenv').config()
+
 const amqp = require('amqplib')
 var axios = require("axios").default;
+const { MongoClient } = require('mongodb');
 
+const TVSERIES_COLLECTION = 'tvseries'
 
-require('dotenv').config()
+async function connectMongo () {
+  const client = new MongoClient(process.env.MONGODB_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true });
+  await client.connect()
+  console.log('CONNECT to mongo')
+  return client.db()
+}
 
 async function getEpisodesByName (name) {
     const {data} = await axios.request({
@@ -21,14 +30,67 @@ async function getEpisodesByName (name) {
         throw new Error('Not Found')
     }
     const {id, numberOfEpisodes, title} = tvSerie
+    const {uniqueId} = id.match(/^\/title\/(?<uniqueId>\w+)\/$/).groups
     return {
-        id,
+        serieId: uniqueId,
         numberOfEpisodes,
         title
     }
 }
 
+async function saveTvSerie (mongoDb, {serieId, numberOfEpisodes, title}) {
+    const date = new Date()
+    await mongoDb.collection(TVSERIES_COLLECTION).updateOne({
+        serieId
+      }, {
+        $set: {
+            serieId,
+            numberOfEpisodes,
+            title,
+            updatedAt: date
+        },
+        $setOnInsert: {
+          createdAt: date
+        }
+      }, {
+        upsert: true
+      })
+}
+
+async function handleConsume (msg, mongoDb) {
+    console.log("[x] Received %s", msg)   
+    const tvSerieDetail = await getEpisodesByName(msg.name)
+    console.log('tvSerieDetail ', tvSerieDetail)
+    await saveTvSerie(mongoDb, tvSerieDetail)
+}
+
+async function initializeCollection (mongoDb) {
+    const createIndex = async () => {
+        console.log('Creating index')
+        await mongoDb.collection(TVSERIES_COLLECTION).createIndex({
+            "serieId": 1
+        },
+        {
+            unique: true
+        })
+    }
+    let indexes = []
+    try {
+        indexes = await mongoDb.collection(TVSERIES_COLLECTION).indexes()
+    } catch (err) {
+        console.log('Collection %s does not exist, it will be created', TVSERIES_COLLECTION)
+        await createIndex()
+        return
+    }
+    if (!indexes.find(index => index.key.serieId)) {
+        await createIndex()
+    }
+}
+
 async function run(){
+    const mongoDb = await connectMongo()
+    await initializeCollection(mongoDb)
+
     const connection = await amqp.connect(process.env.RABBITMQ_CONN_STRING)
     const channel = await connection.createChannel()
     var queue = 'popcorn-planner.tvserie-retrieve';
@@ -42,12 +104,9 @@ async function run(){
     channel.consume(queue, (msg) => {
               console.log('GET MSG')
               console.clear()
-              const msgObj = JSON.parse(msg.content.toString())
-              console.log("[x] Received %s", msgObj)   
-              getEpisodesByName(msgObj.name)
-                .then(tvSerieDetail => {
-                    console.log('tvSerieDetail ', tvSerieDetail)
-                    channel.ack(msg)  
+              handleConsume(JSON.parse(msg.content.toString()), mongoDb)
+                .then(() => {
+                    channel.ack(msg)
                 })
     }, {
         noAck: false
